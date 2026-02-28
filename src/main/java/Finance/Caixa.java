6fda409d3360fe961151223e5bc67c94d1aa8fe0
Thurlex <alex.dias.gold@gmail.com>
2026-02-27 21:46:36 -0300

    Create constructor for Caixa, make getters and setters

--- src/main/java/Finance/Caixa.java
+++ src/main/java/Finance/Caixa.java
@@ -1,4 +1,36 @@
 package Finance;
 
 public class Caixa {
+    private double money;
+    private double tMoney;
+    private String dateOfChange;
+    private double moneyChange;
+
+    public Caixa(double cash, double tCash, String dayOfRetrieval) {
+        this.money = cash;
+        this.tMoney = tCash;
+        this.dateOfChange = dayOfRetrieval;
+    }
+
+    public double getCash() {
+        return money;
+    }
+    public double gettMoney() {
+        return tMoney;
+    }
+    public String getDateOfChange() {
+        return dateOfChange;
+    }
+
+    public void setCash(double cash) {
+        this.money = cash;
+    }
+    public void settMoney(double tMoney) {
+        this.tMoney = tMoney;
+    }
+    public void setDateOfChange(String dateOfChange) {
+        this.dateOfChange = dateOfChange;
+    }
+
+
 }
