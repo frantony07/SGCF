1af39909bcbd319a8dd5cdf0707f640e2603c043
salsichasausage <pacude40metro@gmail.com>
2026-03-02 23:35:28 -0300

    Initializing object Gerente;

--- src/main/java/People/Funcionario.java
+++ src/main/java/People/Funcionario.java
@@ -9,15 +9,22 @@ import java.util.ArrayList;
 public class Funcionario extends People {
 
     private ArrayList<Language> languagesSpoken;
+    private ArrayList<Double> accounts;
 
-
-    public Funcionario(String cpfOrCnpj, int id, String name, ArrayList<Language> languagesSpoken) {
+    public Funcionario(String cpfOrCnpj, int id, String name, ArrayList<Language> languagesSpoken, ArrayList<Double> accounts) {
         super(cpfOrCnpj, id, name);
         this.languagesSpoken = languagesSpoken;
+        this.accounts = accounts;
     }
 
     public ArrayList<Language> getLanguagesSpoken() {return languagesSpoken;}
 
-    public  void addNewLanguage(Language newLanguage){ languagesSpoken.add(newLanguage);}
+    public ArrayList<Double> getAccounts() {
+        return accounts;
+    }
+
+    public void addNewLanguage(Language newLanguage){ languagesSpoken.add(newLanguage);}
+
+
 
 }
