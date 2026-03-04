6a033dc8490d6fe1ab44f5a9a4c01e65fc6cc7d7
Frantony Nieves <frantonynieves@gmail.com>
2026-03-04 20:28:32 -0300

    i created the menu function

--- src/main/java/Functions/functionOfMain.java
+++ src/main/java/Functions/functionOfMain.java
@@ -1,4 +1,31 @@
 package Functions;
 
+import java.util.Scanner;
+
 public class functionOfMain {
+    public  void authenticateUser(){
+        Scanner sc = new Scanner(System.in);
+        String usernameDefine = "admin";
+        String passwordDefine ="1234";
+
+        while (true) {
+            System.out.println("digite seu nome de usuario");
+            String userName = sc.next();
+            System.out.println("digite sua senha");
+            String password = sc.next();
+            if(userName.equals(usernameDefine) && password.equals(passwordDefine)) {
+                return;
+            }
+            System.out.println("usuario ou senha incorreta, tente novamente");
+
+        }
+    }
+    public void menu(){
+        System.out.println("bem vindo ao menu principal");
+        System.out.println("1.cadastrastro");
+        System.out.println("2.agendar reserva");
+        System.out.println("3.mostrar reserva");
+        System.out.println("4.financas");
+    }
+    public void
 }
