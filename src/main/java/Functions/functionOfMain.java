e722499a92ef30380666ad79e5dc190a791cb36e
salsichasausage <pacude40metro@gmail.com>
2026-03-04 20:32:37 -0300

    Create a switch case in menu

--- src/main/java/Functions/functionOfMain.java
+++ src/main/java/Functions/functionOfMain.java
@@ -21,11 +21,36 @@ public class functionOfMain {
         }
     }
     public void menu(){
+        Scanner sc = new Scanner(System.in);
+        int opcaoMenu = 0;
+
         System.out.println("bem vindo ao menu principal");
         System.out.println("1.cadastrastro");
         System.out.println("2.agendar reserva");
         System.out.println("3.mostrar reserva");
         System.out.println("4.financas");
+        opcaoMenu = sc.nextInt();
+
+        switch(opcaoMenu){
+            case 1:
+
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            default:
+                System.out.println("Opção inválida, digite as opções existentes no menu");
+                break;
+        }
+
+
     }
     public void
 }
