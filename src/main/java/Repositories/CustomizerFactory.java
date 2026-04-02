11707cfeb3b5856e65ed294d9b0c7d048c361a5c
salsichasausage <pacude40metro@gmail.com>
2026-04-01 21:04:50 -0300

    I create the paste repositories for db

---
+++ src/main/java/Repositories/CustomizerFactory.java
@@ -0,0 +1,28 @@
+package Repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CustomizerFactory {
+
+    private static final EntityManagerFactory emf;
+
+    static {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+
+        emf = sessionFactory.unwrap(EntityManagerFactory.class);
+    }
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void fechar() {
+        emf.close();
+    }
+
+}
