b9ac239b9620274b87dd87394e21837a1e477cb4
Thurlex <alex.dias.gold@gmail.com>
2026-04-07 19:43:55 -0300

    Change `Pay` under `models` to `ModelLedger` in order to standardadize names

--- src/main/java/org/ONE/models/ModelLedger.java
+++ src/main/java/org/ONE/models/ModelLedger.java
@@ -2,34 +2,31 @@ package org.ONE.models;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Table(name = "pay")
-public class Pay {
+public class ModelLedger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "fk_reservations_id" , nullable = false)
+    @JoinColumn(name = "fk_reservations_id", nullable = false)
     private Reservations reservations;
 
     @Column(name = "total_account")
     private double totalAccount ;
 
-    public Pay( Reservations reservations) {
+    public ModelLedger(Reservations reservations) {
         this.reservations = reservations;
     }
 
-    public Pay() {
+    public ModelLedger() {
     }
 
     @Override
     public String toString() {
         return "\n-----------------------" +
-                "\nPay" +
+                "\nModelLedger" +
                 "\nid=" + id +
                 "\nreservations=" + reservations +
                 "\ntotalAccount=" + totalAccount ;
