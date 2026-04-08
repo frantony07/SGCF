1d15cfb6322a4b158e74d59e921764fe5f56ccd2
Thurlex <alex.dias.gold@gmail.com>
2026-04-08 19:27:38 -0300

    Create version 3 of the database

---
+++ src/main/resources/db/migration/V3__create_tables.sql
@@ -0,0 +1,63 @@
+create table if not exists user_account(
+    id serial not null primary key,
+    user_name varchar(35) unique not null,
+    user_password varchar(35)  not null,
+    permission varchar(30) not null
+);
+
+create table if not exists funcionario(
+    id serial primary key not null,
+    name varchar(50) not null,
+    cpf varchar(11) not null,
+    fk_user_id bigint,
+    foreign key (fk_user_id) references user_account(id)
+);
+
+create table if not exists languages_funcionario(
+    fk_funcionario_id bigint not null,
+    language varchar(50),
+    foreign key (fk_funcionario_id) references funcionario(id)
+);
+
+create table if not exists clientes(
+    id serial not null primary key ,
+    cnpj varchar(15) unique,
+    cpf varchar(11) unique,
+    name varchar(50) not null,
+    country_of_customer varchar(50) not null
+);
+
+create table if not exists clientes_languages(
+    language varchar(50) not null,
+    fk_clientes_id bigint not null,
+    foreign key (fk_clientes_id) references clientes(id)
+);
+
+create table if not exists passeio(
+    id serial not null primary key,
+    price double precision not null,
+    durations_in_minute bigint,
+    country_of_tour varchar(50) not null,
+    km_of_tour bigint,
+    name varchar(100) not null unique,
+    locations varchar(255)
+);
+
+create table if not exists reservations(
+    id serial not null primary key,
+    date time not null,
+    fk_passeio_id bigint not null,
+    fk_funcionario_id bigint not null,
+    fk_clientes_id bigint not null,
+    value double precision not null,
+    foreign key (fk_passeio_id) references passeio(id),
+    foreign key (fk_funcionario_id) references funcionario(id),
+    foreign key (fk_clientes_id) references clientes(id)
+);
+
+create table if not exists pay(
+    id serial not null primary key,
+    status varchar(15) not null,
+    fk_reservation_id bigint not null,
+    foreign key (fk_reservation_id) references reservations(id)
+);
