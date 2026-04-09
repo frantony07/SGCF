540e07bfb4410b0f88c371e10399d007d1dfefd5
salsichasausage <pacude40metro@gmail.com>
2026-04-08 21:27:31 -0300

    create AGAIN the function of editPrice of passeios.

--- src/main/java/org/ONE/repositories/PasseioRepository.java
+++ src/main/java/org/ONE/repositories/PasseioRepository.java
@@ -7,41 +7,39 @@ import java.util.List;
 
 public class PasseioRepository {
 
-    private EntityManager em;
-    public PasseioRepository (EntityManager em) { this.em = em; }
+        private EntityManager em;
+        public PasseioRepository (EntityManager em) { this.em = em; }
 
-    public void create(Passeio passeio) {
-        em.getTransaction().begin();
-        em.persist(passeio);
-        em.getTransaction().commit();
-    }
-
-    public void update(Passeio passeio){
-        em.getTransaction().begin();
-        em.merge(passeio);
-        em.getTransaction().commit();
-    }
-
-    public void delete(Passeio passeio){
-        em.getTransaction().begin();
-        em.remove(em.contains(passeio) ? passeio : em.merge(passeio) );
-        em.getTransaction().commit();
-    }
-
-    public List<Passeio> findAll() { return em.createQuery( "select t from Passeio t", Passeio.class).getResultList();}
-
-    public Passeio findById(Long id) {return em.find(Passeio.class, id); }
-    public List<Passeio> findByName(String prefixo){
-
-        return em.createQuery("select p from Passeio p where lower(p.nome) like lower(:prefixo)", Passeio.class)
-            .setParameter("prefixo", prefixo + "%")
-            .getResultList();
+        public void create(Passeio passeio) {
+            em.getTransaction().begin();
+            em.persist(passeio);
+            em.getTransaction().commit();
         }
 
+        public void update(Passeio passeio){
+            em.getTransaction().begin();
+            em.merge(passeio);
+            em.getTransaction().commit();
+    }
+
+        public void delete(Passeio passeio){
+            em.getTransaction().begin();
+            em.remove(em.contains(passeio) ? passeio : em.merge(passeio) );
+            em.getTransaction().commit();
+        }
+
+        public List<Passeio> findAll() { return em.createQuery( "select t from Passeio t", Passeio.class).getResultList();}
+
+        public Passeio findById(Long id) {return em.find(Passeio.class, id); }
+        public List<Passeio> findByName(String prefixo){
+
+            return em.createQuery("select p from Passeio p where lower(p.nome) like lower(:prefixo)", Passeio.class)
+                    .setParameter("prefixo", prefixo + "%")
+                    .getResultList();
+        }
     public Long getCount(){
         return em.createQuery("select count(t.id) from Passeio t" , Long.class).getSingleResult();
     }
-
     public double getPrice(Long id ){
             return em.createQuery("select t.price from Passeio t where t.id = :id" , Double.class).setParameter("id" , id).getSingleResult();
     }
