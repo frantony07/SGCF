220b4a5e5a18900706486e9d932afdbf6cf7684d
Thurlex <alex.dias.gold@gmail.com>
2026-04-26 13:57:27 -0300

    Create the QuotaServices class based off its sister classes

---
+++ src/main/java/org/ONE/services/QuotaServices.java
@@ -0,0 +1,60 @@
+package org.ONE.services;
+
+import Functions.PrintError;
+import jakarta.persistence.EntityManager;
+import org.ONE.models.QuotaModel;
+import org.ONE.repositories.CustomizerFactory;
+import org.ONE.repositories.QuotaRepository;
+
+import java.util.List;
+
+public class QuotaServices {
+    private EntityManager entityManager = CustomizerFactory.getEntityManager();
+    private QuotaRepository quotaRepository = new QuotaRepository(entityManager);
+
+    public QuotaServices() {};
+
+    public void createQuota() {
+
+    }
+
+    public void updateQuota(QuotaModel quotaModel) {
+        try {
+            if (quotaModel == null) {
+                throw new RuntimeException("A quota não pode ser nula");
+            }
+            quotaRepository.update(quotaModel);
+        } catch (Exception err) {
+            PrintError.printErro(err);
+        }
+    }
+
+    public void deleteQuota(QuotaModel quotaModel) {
+        try {
+            if (quotaModel == null) {
+                throw new RuntimeException("A quota não pode ser nula");
+            }
+            quotaRepository.delete(quotaModel);
+        } catch (Exception err) {
+            PrintError.printErro(err);
+        }
+    }
+
+    public List<QuotaModel> findAllQuotas() {
+        try {
+            return quotaRepository.findAll();
+        } catch (Exception err) {
+            PrintError.printErro(err);
+        }
+        return List.of();
+    }
+
+    public QuotaModel findQuotaById(Long id) {
+        try {
+            return quotaRepository.findById(id);
+        } catch (Exception err) {
+            PrintError.printErro(err);
+        }
+        return null;
+    }
+}
