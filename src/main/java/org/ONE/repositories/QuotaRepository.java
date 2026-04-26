a5dd159ea2f9ab7cab37cd6155e1b9716bccad7c
Thurlex <alex.dias.gold@gmail.com>
2026-04-26 13:39:26 -0300

    Create the basic QuotaRepository class, to be updated with relevant JPQL querries

---
+++ src/main/java/org/ONE/repositories/QuotaRepository.java
@@ -0,0 +1,49 @@
+package org.ONE.repositories;
+
+import jakarta.persistence.EntityManager;
+import org.ONE.models.QuotaModel;
+
+import java.util.List;
+
+public class QuotaRepository {
+    private EntityManager em ;
+
+    public QuotaRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public QuotaModel findById (Long id) {
+        return em.find(QuotaModel.class, id);
+    }
+
+    public void create(QuotaModel quotaModel) {
+        em.getTransaction().begin();
+        em.persist(quotaModel);
+        em.getTransaction().commit();
+    }
+
+    public void update(QuotaModel quotaModel) {
+        em.getTransaction().begin();
+        em.merge(quotaModel);
+        em.getTransaction().commit();
+    }
+
+    public  void delete(QuotaModel quotaModel) {
+        em.getTransaction().begin();
+        em.remove(em.contains(quotaModel) ? quotaModel : em.merge(quotaModel));
+        em.getTransaction().commit();
+    }
+
+    public List<QuotaModel> findByName(String name) {
+        return em.createQuery("select c from QuotaModel c where  lower(c.name) like lower(:name)" , QuotaModel.class).setParameter("name" , name +"%").getResultList();
+    }
+
+    public List<QuotaModel> findAll () {
+        return em.createQuery("select c from QuotaModel c " , QuotaModel.class).getResultList();
+    }
+
+    public Long getSize() {
+        return em.createQuery("select count(c.id) from QuotaModel c" , Long.class).getSingleResult();
+    }
+
+}
