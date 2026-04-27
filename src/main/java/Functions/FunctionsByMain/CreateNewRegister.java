1f711c9b0bbb2ec224d62cd265c686e931ee30a4
Thurlex <alex.dias.gold@gmail.com>
2026-04-27 19:38:14 -0300

    Minor bug fixes on V9

--- src/main/java/Functions/FunctionsByMain/CreateNewRegister.java
+++ src/main/java/Functions/FunctionsByMain/CreateNewRegister.java
@@ -29,7 +29,7 @@ public class CreateNewRegister {
                 System.out.println("1. Criar novo funcionário");
                 System.out.println("2. Criar novo cliente");
                 System.out.println("3. Criar novo passeio");
-                System.out.println("4. criar novo usuario ");
+                System.out.println("4. Criar novo usuario ");
                 System.out.println("5. Voltar ao menu principal");
                 int menuOption = ValidateNumber.validateINT(5);
 
