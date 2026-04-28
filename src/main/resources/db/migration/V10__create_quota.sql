f392efa99b02638523001a3913b157f48932f8b7
Thurlex <alex.dias.gold@gmail.com>
2026-04-28 20:45:01 -0300

    Fix the most minor misspelling EVER

--- src/main/resources/db/migration/V10__create_quota.sql
+++ src/main/resources/db/migration/V10__create_quota.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS meta_funcionario (
     end_date date,
     target_value double precision not null,
     accumulated_value double precision not null,
-    fk_reservation_id serial not null,
+    fk_reservation_id serial,
     fk_funcionario_id serial,
 
-    foreign key (fk_funcionario_id) references funcionarios(id)
+    foreign key (fk_funcionario_id) references funcionario(id)
 );
