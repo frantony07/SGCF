3ec6acf149b22ed317c823b8ab1de9bc6fcf9308
Thurlex <alex.dias.gold@gmail.com>
2026-04-29 16:33:39 -0300

    Update V10 (there was a massive bug there)

--- src/main/resources/db/migration/V10__create_quota.sql
+++ src/main/resources/db/migration/V10__create_quota.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS meta_funcionario (
     id serial not null,
     start_date date not null,
-    end_date date,
+    end_date date not null,
     target_value double precision not null,
-    accumulated_value double precision not null,
-    fk_reservation_id serial,
-    fk_funcionario_id serial,
+    fk_funcionario_id bigint,
 
     foreign key (fk_funcionario_id) references funcionario(id)
 );
