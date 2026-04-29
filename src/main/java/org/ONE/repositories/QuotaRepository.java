8e51f84e49a8055f58b481ce8027a7510b41f4e6
Thurlex <alex.dias.gold@gmail.com>
2026-04-29 17:05:56 -0300

    Disambiguate names for the QuotaRepository

--- src/main/java/org/ONE/repositories/QuotaRepository.java
+++ src/main/java/org/ONE/repositories/QuotaRepository.java
@@ -16,30 +16,29 @@ public class QuotaRepository {
         return em.find(QuotaModel.class, id);
     }
 
-    public void create(QuotaModel quotaModel) {
+    public void createQuota(QuotaModel quotaModel) {
         em.getTransaction().begin();
         em.persist(quotaModel);
         em.getTransaction().commit();
     }
 
-    public void update(QuotaModel quotaModel) {
+    public void updateQuota(QuotaModel quotaModel) {
         em.getTransaction().begin();
         em.merge(quotaModel);
         em.getTransaction().commit();
     }
 
-    public  void delete(QuotaModel quotaModel) {
+    public  void deleteQuota(QuotaModel quotaModel) {
         em.getTransaction().begin();
         em.remove(em.contains(quotaModel) ? quotaModel : em.merge(quotaModel));
         em.getTransaction().commit();
     }
 
-    public List<QuotaModel> findAll () {
+    public List<QuotaModel> findAllQuotas() {
         return em.createQuery("select c from QuotaModel c " , QuotaModel.class).getResultList();
     }
 
     public Long getSize() {
         return em.createQuery("select count(c.id) from QuotaModel c" , Long.class).getSingleResult();
     }
-
 }
