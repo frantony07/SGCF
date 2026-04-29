1dd82ef9764bc790d0449e212bb6e32811cd883d
Thurlex <alex.dias.gold@gmail.com>
2026-04-29 17:08:46 -0300

    Update V10 (I know it's wrong) as using time instead of timestamp was apparently killing the used of LocalTimeDate

--- src/main/resources/db/migration/V10__create_quota.sql
+++ src/main/resources/db/migration/V10__create_quota.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS meta_funcionario (
+CREATE TABLE IF NOT EXISTS meta (
     id serial not null,
-    start_date date not null,
-    end_date date not null,
+    start_date timestamp not null,
+    end_date timestamp not null,
     target_value double precision not null,
-    fk_funcionario_id bigint,
+    fk_funcionario_id bigint null,
 
     foreign key (fk_funcionario_id) references funcionario(id)
 );
