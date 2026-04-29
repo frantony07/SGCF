bdb21f31f0552b31442e0885cc18ee48e45b9905
Thurlex <alex.dias.gold@gmail.com>
2026-04-29 19:45:23 -0300

    Forgot to delete the big functions off QuotaSelection

--- src/main/java/Finance/QuotaSelection.java
+++ src/main/java/Finance/QuotaSelection.java
@@ -1,12 +1,11 @@
 package Finance;
 
+import Finance.QuotaFunctions.CreateQuotas;
+import Finance.QuotaFunctions.PrintQuotaInformation;
 import Functions.PrintError;
 import Functions.ValidateNumber;
-import org.ONE.models.QuotaModel;
-import org.ONE.services.FuncionarioServices;
 import org.ONE.services.QuotaServices;
 
-import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class QuotaSelection {
@@ -26,14 +25,13 @@ public class QuotaSelection {
 
                 switch (metaOption) {
                     case 1:
-                        createEmployeeQuota(quotaServices, sc);
+                        new CreateQuotas().createEmployeeQuota(quotaServices, sc);
                         break;
                     case 2:
-                        createCompanyQuota(quotaServices, sc);
+                        new CreateQuotas().createCompanyQuota(quotaServices, sc);
                         break;
                     case 3:
-                        // Start work
-                        System.out.println("Metas ativas: ");
+                        new PrintQuotaInformation().printEmployeeEarnings(quotaServices, sc);
                         break;
                     case 4:
                         System.out.println("Retornando ao menu financeiro...");
@@ -46,65 +44,5 @@ public class QuotaSelection {
         } catch (Exception err) {
             PrintError.printErro(err);
         }
-    }
-
-    public void createEmployeeQuota(QuotaServices quotaServices, Scanner sc) {
-        FuncionarioServices funcionarioServices = new FuncionarioServices();
-
-        System.out.println("Digite o CPF do funcionário: ");
-        String cpf = sc.next();
-        Long employeeID = funcionarioServices.findIdByCPF(cpf);
-
-        if (employeeID == null) {
-            System.out.println("Não foi encontrado nenhum funcionário com este CPF.");
-            return;
-        }
-
-        System.out.println("Insira o valor da sua meta: ");
-        while (!sc.hasNextDouble()) {
-            System.out.println("Digite um valor válido.");
-            sc.next();
-        }
-        double employeeTargetValue = sc.nextDouble();
-
-        System.out.println("Valor da meta criada: " + employeeTargetValue);
-        LocalDateTime employeeInitialDate = LocalDateTime.now();
-        LocalDateTime employeeFinalDate = employeeInitialDate.plusDays(30);
-        System.out.println("A data limite para a meta é de 30 dias em: " + employeeFinalDate);
-
-        quotaServices.createQuota(
-                new QuotaModel(
-                        employeeInitialDate,
-                        employeeFinalDate,
-                        employeeTargetValue,
-                        employeeID
-                )
-        );
-    }
-
-    public void createCompanyQuota(QuotaServices quotaServices, Scanner sc) {
-        System.out.println("Insira o valor da meta da empresa: ");
-        while (!sc.hasNextDouble()) {
-            System.out.println("Digite um valor válido.");
-            sc.next();
-        }
-        double companyTargetValue = sc.nextDouble();
-
-        System.out.println("Valor da meta criada: " + companyTargetValue);
-        LocalDateTime companyInitialDate = LocalDateTime.now();
-        LocalDateTime companyFinalDate = companyInitialDate.plusDays(30);
-        System.out.println("A data limite para a meta é de 30 dias em: " + companyFinalDate);
-
-        quotaServices.createQuota(
-                new QuotaModel(
-                        companyInitialDate,
-                        companyFinalDate,
-                        companyTargetValue
-                )
-        );
-    }
-
-    public void printQuotas(QuotaServices quotaServices) {
-
     }
 }
