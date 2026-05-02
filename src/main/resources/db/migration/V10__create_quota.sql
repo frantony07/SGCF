849f52333434ec0fbca570adef56052f339a859b
Thurlex <alex.dias.gold@gmail.com>
2026-05-01 23:23:55 -0300

    Update V10, will have to drop the meta table to get flyway to work again

--- src/main/resources/db/migration/V10__create_quota.sql
+++ src/main/resources/db/migration/V10__create_quota.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS meta (
     id serial not null,
-    start_date timestamp not null,
-    end_date timestamp not null,
+    start_date date not null,
+    end_date date not null,
     target_value double precision not null,
     fk_funcionario_id bigint null,
 
